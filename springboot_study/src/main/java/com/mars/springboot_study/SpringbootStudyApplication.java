6b8af560ee9e7e849eadee90f01c77f58dee8ee8
Mars <1051996322@qq.com>
2023-05-28 14:25:55 +0800

    添加日志 add by Mars

--- springboot_study/src/main/java/com/mars/springboot_study/SpringbootStudyApplication.java
+++ springboot_study/src/main/java/com/mars/springboot_study/SpringbootStudyApplication.java
@@ -9,6 +9,7 @@ public class SpringbootStudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStudyApplication.class, args);
         System.out.println("springboot project run suncess!");
+        System.out.println("更新远程仓库代码");
     }
 
 }
